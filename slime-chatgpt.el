84588ffb94f8de0918c4ecbcd1e90a2ebb26a427
pve1 <pve1@foo.invalid>
2023-03-09 11:56:10 +0200

    Added slime-chatgpt-consult.

--- slime-chatgpt.el
+++ slime-chatgpt.el
@@ -1,6 +1,10 @@
-;;; This file provides the function
-;;; 'slime-chatgpt-comment-symbol-at-point' that generates lisp
-;;; comments for functions using ChatGPT.
+;;; This file provides the functions
+
+;; 'slime-chatgpt-comment-symbol-at-point'
+;;   - Generates comments for functions using ChatGPT.
+
+;; 'slime-chatgpt-consult'
+;;   - Answers general questions about the current buffer.
 
 ;;; IN ORDER TO GET REASONABLE RESULTS, THE ENTIRE BUFFER IS SENT TO
 ;;; CHATGPT AS CONTEXT. DO NOT USE THIS ON CODE YOU WANT TO
@@ -16,10 +20,11 @@
 ;;; 3. jq (for parsing the response)
 
 (require 'slime)
+(require 'ido)
 (require 'cl-lib)
 
 (defgroup slime-chatgpt nil
-  "Generate comments using ChatGPT."
+  "Answer questions using ChatGPT."
   :prefix "slime-chatgpt-"
   :group 'slime)
 
@@ -35,6 +40,15 @@ Please set it with \
 (defcustom slime-chatgpt-question-format-string "Can you produce a brief comment for the function '%s'?"
   "The question we want to ask ChatGPT as a format string."
   :type '(string)
+  :group 'slime-chatgpt)
+
+(defcustom slime-chatgpt-consult-questions
+  '("Could you...?"
+    "Could you write a function that...?"
+    "Could you describe the purpose of...?"
+    "Could you write a few test cases for the function '...'?")
+  "Example questions we may want to ask ChatGPT."
+  :type '(list)
   :group 'slime-chatgpt)
 
 (defcustom slime-chatgpt-api-endpoint "https://api.openai.com/v1/chat/completions"
@@ -53,8 +67,12 @@ File contents:
   :type '(string)
   :group 'slime-chatgpt)
 
+(defvar slime-chatgpt-generate-question-function nil)
+
 (defun slime-chatgpt-generate-question ()
-  (format slime-chatgpt-question-format-string (symbol-at-point)))
+  (if slime-chatgpt-generate-question-function
+      (funcall slime-chatgpt-generate-question-function)
+    (format slime-chatgpt-question-format-string (symbol-at-point))))
 
 (defun slime-chatgpt-generate-prompt ()
   (format slime-chatgpt-prompt-format-string
@@ -65,11 +83,11 @@ File contents:
 
 (defun slime-chatgpt-generate-prompt-interactively ()
   (let* ((initial (slime-chatgpt-generate-question))
-         (question 
-         (read-from-minibuffer "Question: " 
+         (question
+         (read-from-minibuffer "Question: "
                                initial
                                nil
-                               nil 
+                               nil
                                'slime-chatgpt-question-history)))
     (format slime-chatgpt-prompt-format-string
             question
@@ -82,7 +100,7 @@ File contents:
 ;; Single quote is not considered a separator.
 (defun slime-chatgpt-single-quoted-string-for-shell (string)
   (replace-regexp-in-string "'" "'\\\\''"  string))
- 
+
 (defun slime-chatgpt-build-curl-command ()
   (format "curl '%s' \
 -s \
@@ -100,7 +118,7 @@ File contents:
     (let* ((url-request-method "POST")
            (url-request-extra-headers
             `(("Content-Type" . "application/json")
-              ("Authorization" . ,slime-chatgpt-api-key))) 
+              ("Authorization" . ,slime-chatgpt-api-key)))
            (curl-buffer (get-buffer-create "*slime-chatgpt curl*"))
            (prompt-buffer (get-buffer-create "*slime-chatgpt prompt*"))
            (response-buffer (get-buffer-create "*slime-chatgpt response*"))
@@ -117,34 +135,37 @@ File contents:
       (insert json)
       (message "Consulting ChatGPT, please wait.")
       (unless dry-run
-        (shell-command-on-region 1 
+        (shell-command-on-region 1
                                  (buffer-end 1)
                                  command
                                  response-buffer
                                  nil
-                                 "*slime-chatgpt error*"))    
+                                 "*slime-chatgpt error*"))
       (message "Consulting ChatGPT, please wait. Done."))))
 
 (defun slime-chatgpt-parse-response ()
   (save-excursion
     (set-buffer "*slime-chatgpt response*")
-    (shell-command-on-region 1 
-                             (buffer-end 1) 
+    (shell-command-on-region 1
+                             (buffer-end 1)
                              "jq '.choices[0].message.content'")
     (set-buffer "*Shell Command Output*")
-    (car 
+    (car
      (read-from-string
       (buffer-substring-no-properties 1 (buffer-end 1))))))
+
+(defvar slime-chatgpt-format-response-comment t)
 
 (defun slime-chatgpt-format-response ()
   (save-excursion
     (let ((message (slime-chatgpt-parse-response))
           (buffer (get-buffer-create "*slime-chatgpt comment*")))
       (set-buffer buffer)
-      (erase-buffer) 
+      (erase-buffer)
       (setf message (replace-regexp-in-string "^\n*" "" message))
       (insert message)
-      (replace-regexp "^" ";; " nil 1 (buffer-end 1)) 
+      (when slime-chatgpt-format-response-comment
+        (replace-regexp "^" ";; " nil 1 (buffer-end 1)))
       (buffer-substring-no-properties 1 (buffer-end 1)))))
 
 (defun slime-chatgpt-insert-response ()
@@ -153,18 +174,34 @@ File contents:
     (insert "\n")
     (previous-line)
     (beginning-of-line)
-    (save-excursion  
+    (save-excursion
       (insert contents))))
 
-(defun slime-chatgpt-comment-symbol-at-point ()
-  (interactive)
+(defun slime-chatgpt-clear-temporary-buffers ()
   (dolist (buf '("*slime-chatgpt error*"
                  "*slime-chatgpt response*"
                  "*slime-chatgpt curl*"
                  "*slime-chatgpt comment*"))
     (when (buffer-live-p (get-buffer buf))
-      (kill-buffer buf)))
+      (kill-buffer buf))))
+
+(defun slime-chatgpt-comment-symbol-at-point ()
+  (interactive)
+  (slime-chatgpt-clear-temporary-buffers)
   (slime-chatgpt-send-post-request)
   (slime-chatgpt-insert-response))
+
+(defun slime-chatgpt-consult ()
+  (interactive)
+  (slime-chatgpt-clear-temporary-buffers)
+  (let* ((slime-chatgpt-format-response-comment nil)
+         (question (if slime-chatgpt-consult-questions
+                       (ido-completing-read "" slime-chatgpt-consult-questions)
+                     "?"))
+         (slime-chatgpt-generate-question-function
+          (lambda ()
+            question)))
+    (slime-chatgpt-send-post-request)
+    (slime-chatgpt-insert-response)))
 
 (provide 'slime-chatgpt)
